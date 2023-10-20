ee7f6aa6b2788e56170a5120b53590ddbe3e992d
Baylee <baylee.hillmer@nwtkc.edu>
2023-10-20 11:33:05 -0600

    yes

---
+++ src/whale/whale.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-whale',
+  templateUrl: './whale.component.html',
+  styleUrls: ['./whale.component.css']
+})
+export class WhaleComponent {
+  isDancing = false;
+
+
+
+
+
+  startDancing() {
+    this.isDancing = true;
+  }
+}
